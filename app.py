78650b26c80ec0f9ecac0e4fc6833d5757969dbf
danukumar111999 <danukumar111999@gmail.com>
2024-06-25 10:06:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def main():
         text_input = text_input.replace(" ", "")  # This line removes spaces from the input sequence
         prediction_option = st.radio("\n 1.Remove the spaces from the amino acid sequences:\n"
                                      "\n 2.Remove Headers/Identifier\n"
-                                     "\n 3.If the input protein sequence is more than 250 amino acids, we recommend you to use our standalone package through this link https://drive.google.com/file/d/1H3-yHwCJ7GQ2Nxwol4zoqsvQ_Pe2qjgr/view?usp=drive_link(See help page for more information):\n"
+                                     "\n 3.If the input protein sequence is more than 250 amino acids, we recommend you to use our standalone package through this link https://drive.google.com/file/d/1H3-yHwCJ7GQ2Nxwol4zoqsvQ_Pe2qjgr/view?usp=drive_link( See help page for more information ):\n"
                                      "\n 4.Dont close the browser while Analysis is Running\n",
                                      ("MHC-1", "MHC-2", "BOTH"))
 
